9007914af878d4567ee8ee3ecb9a899f868437b2
tejeoner34 <atzarco37@gmail.com>
2022-08-25 12:41:06 +0200

    cors policy updated

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const io = new Server(server, {
   },
 });
 
-const allowedOrigins = ["http://localhost:4200"];
+const allowedOrigins = ["https://startling-cupcake-ce0148.netlify.app/"];
 const corsOptions = {
   origin: allowedOrigins,
 };
