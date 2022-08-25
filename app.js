d6af6ba3021b4c6b8f96f6a1d9a4577da67103cf
tejeoner34 <atzarco37@gmail.com>
2022-08-25 12:46:32 +0200

    fixing cors problem with origin url

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const io = new Server(server, {
   },
 });
 
-const allowedOrigins = ["https://startling-cupcake-ce0148.netlify.app/"];
+const allowedOrigins = ["https://startling-cupcake-ce0148.netlify.app"];
 const corsOptions = {
   origin: allowedOrigins,
 };
